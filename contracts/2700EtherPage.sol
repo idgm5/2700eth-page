a2bca6d38e62f237f84c1898b8481839c2fc3491
idgm5 <idgm@pm.me>
2020-09-13 16:30:49 -0500

    Add refund logic if outbidded, withdraw function

--- contracts/2700EtherPage.sol
+++ contracts/2700EtherPage.sol
@@ -8,6 +8,7 @@ contract TwentySevenHundredEtherPage {
   }
   
   Pixel[100][1000] public pixels;
+  mapping(address => uint) public pendingRefunds;
   
   event PixelChanged(
       uint x,
@@ -20,11 +21,26 @@ contract TwentySevenHundredEtherPage {
   function colorPixel(uint x, uint y, bytes3 color) payable public {
     Pixel storage pixel = pixels[x][y];
     require(msg.value > pixel.soldPrice);
-   
+    
+    if(pixel.owner != address(0x0)) {
+        pendingRefunds[pixel.owner] += pixel.soldPrice;
+    }
+    
     pixel.owner = msg.sender;
     pixel.soldPrice = msg.value;
     pixel.color = color;
     
     emit PixelChanged(x, y, pixel.owner, pixel.soldPrice, pixel.color);
+  }
+  
+  function withdrawRefunds() public {
+      address payable payee = msg.sender;
+      uint payment = pendingRefunds[payee];
+      
+      require(payment != 0);
+      require(address(this).balance >= payment);
+      
+      pendingRefunds[payee] = 0;
+      require(payee.send(payment));
   }
 }
